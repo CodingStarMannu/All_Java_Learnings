dd239d771a19c0d7de4d7d02261f2bbd1f41f0b9
Manoj_Pant <manojpant097@gmail.com>
2023-03-22 19:07:31 +0530

    New Commit

---
+++ firstproject/Arraydecl.java
@@ -0,0 +1,29 @@
+package firstproject;
+
+public class Arraydecl {
+
+	public static void main(String[] args) {
+//		int arr[]= new int [10];
+////		int[] arr= new int [10];//both syntax are used to declare an array
+//		arr[0]= 5;
+//		arr[5]= 17;
+//		System.out.println(arr[0]);
+//		System.out.println(arr[5]);
+//		System.out.println(arr[3]);
+//		System.out.println(arr[6]);
+//		
+//		
+////		arr[-1]=18 // negative indexing is  not allowed in array.
+////		
+//		char[] cArray= new char[10];
+//		double[]dArray = new double[10];
+//		System.out.println(cArray[3]);
+//		System.out.println(dArray[5]);
+		
+//		char chArray[] = new char[15];
+//		System.out.println(chArray[15]);//here indexing is from 0 to 14 so it will error index out of range
+		boolean arr[] = new boolean[5];
+		System.out.println(arr[4]);
+	}
+
+}
